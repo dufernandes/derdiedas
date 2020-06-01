bb3a918b0f54556d2eae447193bc91a6fe10d096
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2020-06-01 23:40:22 +0200

    small refactory

--- src/main/java/com/derdiedas/authentication/JwtUtils.java
+++ src/main/java/com/derdiedas/authentication/JwtUtils.java
@@ -1,32 +1,35 @@
 package com.derdiedas.authentication;
 
-import com.auth0.jwt.JWT;
-import com.derdiedas.model.User;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.Date;
-
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static com.derdiedas.authentication.SecurityConstants.SECRET;
 import static com.derdiedas.authentication.SecurityConstants.TOKEN_PREFIX;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
+import com.auth0.jwt.JWT;
+import com.derdiedas.model.User;
+
+import java.util.Date;
+
 class JwtUtils {
 
-    static String createJWTToken(User user) {
-        return user != null && isNotEmpty(user.getUsername())
-                ? JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
-                .sign(HMAC512(SECRET.getBytes()))
-                : null;
-    }
+  private JwtUtils() {
+    // private constructor to avoid class instantiation
+  }
+
+  static String createJWTToken(User user) {
+    return user != null && isNotEmpty(user.getUsername())
+        ? JWT.create()
+        .withSubject(user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+        .sign(HMAC512(SECRET.getBytes()))
+        : null;
+  }
 
 
-    static String extractUserPrincipalFromJwtToken(String token) {
-        return isNotEmpty(token) ? JWT.require(HMAC512(SECRET.getBytes()))
-                .build()
-                .verify(token.replace(TOKEN_PREFIX, ""))
-                .getSubject() : null;
-    }
+  static String extractUserPrincipalFromJwtToken(String token) {
+    return isNotEmpty(token) ? JWT.require(HMAC512(SECRET.getBytes()))
+        .build()
+        .verify(token.replace(TOKEN_PREFIX, ""))
+        .getSubject() : null;
+  }
 }
