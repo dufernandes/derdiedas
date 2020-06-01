8e3c4cf02e31cf4279f9c5c99a3ced8c969c89aa
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2020-06-01 19:07:19 +0200

    Added joavadoc to root page

--- src/main/java/com/derdiedas/controller/RootController.java
+++ src/main/java/com/derdiedas/controller/RootController.java
@@ -14,6 +14,6 @@ public class RootController {
 
     @GetMapping(path = "")
     public String home() {
-        return "Welcome to the DerDieDas software. Please access our <a href=\"/docs/api/index.html\">API documentation</a> for using understanding our software.";
+        return "Welcome to the DerDieDas software. Please access our <a href=\"/docs/api/index.html\">API documentation</a> for using understanding our software. You can also access our <a href=\"/docs/apidocs/index.html\">Javadoc here.</a>.";
     }
 }
